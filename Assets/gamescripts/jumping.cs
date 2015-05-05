e6b17e964d508ab3bb83a6f99e7307ff3ba2ffd8
Afzonia-Khizar <afzonia.khizar@gmail.com>
2015-05-06 01:06:01 +0500

    Terrain environment

---
+++ Assets/gamescripts/jumping.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class jumping : MonoBehaviour {
+    public GameObject CharacterMotor;
+    private CharacterMotor personcharacter;
+
+	// Use this for initialization
+	void Start () {
+        personcharacter = CharacterMotor.GetComponent<CharacterMotor>();
+        personcharacter.jumping.baseHeight = 15;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/gamescripts/running.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class running : MonoBehaviour {
+    public GameObject CharacterMotor;
+    private CharacterMotor personcharacter;
+
+	// Use this for initialization
+	void Start () {
+        personcharacter = CharacterMotor.GetComponent<CharacterMotor>();
+        personcharacter.movement.maxForwardSpeed= 300;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
