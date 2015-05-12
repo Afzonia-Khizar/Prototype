b0e4d8b009ebe7654d1361d696f6e59cd34dad51
Afzonia-Khizar <afzonia.khizar@gmail.com>
2015-05-12 10:10:33 +0500

    scenes with scripting

---
+++ Assets/gamescripts/gui test.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class guitest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void ongui()
+    {
+        if (GUI.Button(new Rect (10,10,100,150), "Button"))
+        {
+            print("clicked");
+        }
+    }
+
+   
+}
